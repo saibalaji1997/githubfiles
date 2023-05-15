a5747ed3acb8af689cfb8c1455d54a57b8287cce
saibalaji1997 <114025759+saibalaji1997@users.noreply.github.com>
2023-05-15 04:04:31 -0700

    Joystick.c

--- TIVAC/Assignment 1/Joystick/Joystick.c
+++ TIVAC/Assignment 1/Joystick/Joystick.c
@@ -5,67 +5,87 @@
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/timer.h"
+#include "driverlib/debug.h"
+#include "driverlib/adc.h"
+#include "utils/uartstdio.h"
+#include <string.h>
 
-// Function prototypes
-void ConfigureTimer(void);
-void Timer0IntHandler(void);
 
-int main(void)
+#ifdef DEBUG
+void__error__(char *pcFilename, uint32_t ui32Line)
 {
-    // Set the clock frequency
-    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+}
+#endif
 
-    // Enable the peripherals
+// Globals
+uint32_t ui32ADC0Value[2];
+volatile uint32_t ui32XAvg;
+volatile uint32_t ui32YAvg;
+
+
+// Timer 1 ISR
+void Timer1IntHandler(void)
+{
+     // Clear the timer interrupt
+     TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+
+     ADCIntClear(ADC0_BASE, 3);
+     ADCProcessorTrigger(ADC0_BASE, 3);
+
+     ADCSequenceDataGet(ADC0_BASE, 3, ui32ADC0Value);
+
+     ui32XAvg = (ui32ADC0Value[0] + 2)/4;
+     ui32YAvg = (ui32ADC0Value[1] + 2)/4;
+
+     UARTprintf("VRx: %d \t VRy: %d\n", ui32XAvg, ui32YAvg);
+}
+
+
+
+int main(void) {
+
+    // Configure Clock
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    // Configure peripherals
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);  // Enabling Timer 1
 
-    // Configure the timer interrupt
-    ConfigureTimer();
+    // Configure ADC
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    ADCHardwareOversampleConfigure(ADC0_BASE, 32);
 
-    // Enable the timer interrupt
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    IntEnable(INT_TIMER0A);
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
 
-    // Configure the joystick pins as input
-    GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_2 | GPIO_PIN_3);
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH11);  // VRx input
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 1, ADC_CTL_CH9);   // VRy input
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 2, ADC_CTL_TS|ADC_CTL_IE|ADC_CTL_END);
 
-    // Enable the interrupt
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+////////////////////
+    // Configure Timer 1 module
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+    uint32_t ui32Period = SysCtlClockGet()/2;   // Period of 0.5s 2Hz
+    TimerLoadSet(TIMER1_BASE, TIMER_A, ui32Period -1);
+    IntEnable(INT_TIMER1A);
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+    TimerEnable(TIMER1_BASE, TIMER_A);
+
+    // Configure UART
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTStdioConfig(0, 115200, SysCtlClockGet());
+
+    // Enable processor interrupts
     IntMasterEnable();
 
-    // Loop forever
-    while(1)
-    {
-        // Read the joystick values
-        uint32_t xVal = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_2);
-        uint32_t yVal = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_3);
+    while (1) {
 
-        // Print the values to the terminal
-        printf("X: %d, Y: %d\n", xVal, yVal);
     }
-}
 
-// Configure the timer interrupt
-void ConfigureTimer(void)
-{
-    // Set the timer period
-    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-    TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet()/2);
-
-    // Enable the timer
-    TimerEnable(TIMER0_BASE, TIMER_A);
-}
-
-// Timer interrupt handler
-void Timer0IntHandler(void)
-{
-    // Clear the interrupt
-    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-
-    // Read the joystick values
-    uint32_t xVal = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_2);
-    uint32_t yVal = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_3);
-
-    // Print the values to the terminal
-    printf("X: %d, Y: %d\n",
+    return 0;
 }
